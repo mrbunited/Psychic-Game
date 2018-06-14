c0dfc93e0b039843da42d31bfc559cf2310b3e07
mradke2 <mrbunited@gmail.com>
2018-06-13 22:05:58 -0500

    061318

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,12 @@
 
 // define all possible values for computer choice
 
-var emptyString = "";
 var computerChoices = "abcdefghijklmnopqrstuvwxyz";
 var userOption = "abcdefghijklmnopqrstuvwxyz";
 var getRandomLetter = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 var randomLetter = getRandomLetter;
-console.log(getRandomLetter);
 
+// console.log(getRandomLetter);
 
 // create an empty array for recording/storing all userGuesses
 var guessesLeft =9;
@@ -20,57 +19,43 @@ var wins = 0;
 var losses = 0;
 
 //  key function
-
-
-// created a function startgame, included variables  and previouslyGuessedLetters = []
-
-// then hiddenletter = the variable for the alphabet string [math.floor(math.random()* (alphabet variable again.length)]
-// // then console logged("message", hiddenletter);
-// // and then function updateDocument
-
-// function updateDocument(
-//     for (var i = 0; i < 1; i--) {
-//         guessesLeft.push(event.key);
-//         console.log(guessesLeft);
-    
-//      }  
-
-
+ 
 document.onkeyup = function(event){
     var userGuess = event.key;
     var userGuessLower = userGuess.toLowerCase();
 
     if (userOption.indexOf(userGuessLower)!==-1)
-    {
+{
       if (userGuessLower === getRandomLetter) {
-        wins++;
-    } else if (userGuessLower !== getRandomLetter)  {losses++ ;
+        wins++; guessesLeft =9;
+        storeUserGuess =[];
+        getRandomLetter = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+    } else{guessesLeft--;
+    }
+    if (guessesLeft === 0)  {
+        losses++ ;
+        guessesLeft =9;
+        storeUserGuess =[];
+        getRandomLetter = computerChoices[Math.floor(Math.random() * computerChoices.length)];
     }
 
-    
-    for (var i = 0; i < 1; i++) {
-      
+   
     storeUserGuess.push(event.key);
 
-    console.log(storeUserGuess);
-    }}
+    // console.log(storeUserGuess);
+    }
  
-
-//    when (storeUserGuess.length < 10) {
-//         storeUserGuess = []
-//         console.log(storeUserGuess)
-//  }
-// // loop for comparing user input and computer guess 9 times
 
 // // display results
 var html =
-"<p>The computer chose: " + getRandomLetter + "</p>" +
 "<p>wins: " + wins + "</p>" +
 "<p>losses: " + losses + "</p>" +
 "<p>You chose: " + storeUserGuess + " ," + "</p>" ;
 
 // // Set the inner HTML contents of the #game div to our html string
 document.querySelector("#game").innerHTML = html;
+
 };
 
 
